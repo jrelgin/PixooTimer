b6e86c07d9058acbfd68483068ff430922896584
Jason Elgin <jason@signllantern.com>
2026-01-08 08:28:33 -0500

    Initial Commit

---
+++ PixooTimer/PixooTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixooTimerApp.swift
+//  PixooTimer
+//
+//  Created by Jason Elgin on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct PixooTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
